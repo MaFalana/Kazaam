320c237daa80b82d9c455798695231167f2d8fe1
Falana, Malik <malikfalana@icloud.com>
2022-10-10 12:40:54 -0400

    2nd commit

--- Kazaam/Snippet Manager.swift
+++ Kazaam/Snippet Manager.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+
+class Snippet_Manager: ObservableObject
+{
+    var searchResult: Result
+    
+    func grabSnippet()
+    {
+        
+    }
+    
+    func storeSnippet()
+    {
+        
+    }
+    
+    func Snippet_to_String() -> String //Method that converts Snippet to a String
+    {
+        
+    }
+    
+    func search_IDMb() -> Result //Method that searches IMDb with string
+    {
+        
+    }
+    
+    func is_Result() -> Bool //Method to check if search result is not a null value
+    {
+        
+    }
+    
+    func showResults()
+    {
+        
+    }
+    
+    func openPlatform()
+    {
+        
+    }
+}
+
+
+struct Result: Codable
+{
+    let Title: String
+    let Cover: String
+    let Synopsis: String
+    let Genre: String
+}
