615fbb43431d56696c944efe3f1fe7b20e84368d
Falana, Malik <malikfalana@icloud.com>
2022-10-10 12:23:44 -0400

    Initial Commit

---
+++ Kazaam/KazaamApp.swift
@@ -0,0 +1,17 @@
+//
+//  KazaamApp.swift
+//  Kazaam
+//
+//  Created by Malik Falana on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct KazaamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
